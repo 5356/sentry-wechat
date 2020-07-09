6ec3849d321a28a1ba2399b3c1e150feaaac0b5f
jerryhu <jerry_hu123@qq.com>
2020-07-09 16:19:05 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     ],
     entry_points={
         'sentry.plugins': [
-            'wechat_prod = sentry_wechat.plugin:WechatProdPlugin'
+            'wechat_prod = sentry_wechat_prod.plugin:WechatProdPlugin'
         ]
     },
     classifiers=[
